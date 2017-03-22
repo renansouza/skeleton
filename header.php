29df6ac8a958fd4fda5792999086a0b9b6c7b121
Renan Souza <falecomrenansouza@gmail.com>
2017-03-22 04:08:28 -0300

    Configurações Básicas dos arquivos do tema

--- header.php
+++ header.php
@@ -2,8 +2,9 @@
 <html lang="pt-BR">
 <head>
 	<meta charset="UTF-8">
-	<title>Untitled document</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title><?php bloginfo('name'); ?></title>
+	
 
 	<?php wp_head();?>
 </head>
